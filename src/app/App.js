88d39cdac9385ba82642fd1a7af24b504a0b0741
Sean Byrd <byrd.sean1015@gmail.com>
2022-07-17 00:47:41 -0400

    Refactored puzzle datatype to be a flat array instead of multidimensional. This makes it easier to update in state. Commented validation functions for now. Added .vscode folder to gitignore.

--- src/app/App.js
+++ src/app/App.js
@@ -7,78 +7,165 @@ function App() {
   const maximumValue = 9;
   const squareSize = 3;
   const uniqueValues = new Set();
+
+  //Flatten puzzle board
   const [puzzleBoard, setPuzzleBoard] = useState([]);
 
-  const startSolver = () => {};
+  //Flag to know if solver has started
+  const [started, setStarted] = useState(false);
 
-  //For a given row and col coordinate, validate the 3x3 square
-  const isValidSquare = (row, col) => {
+  //Next index to insert an answer
+  const [insertAnswerIndex, setInsertAnswerIndex] = useState(0);
+
+  //Handle for setInterval
+  const [loopHandle, setLoopHandle] = useState(0);
+
+  // //For a given row and col coordinate, validate the 3x3 square
+  // const isValidSquare = (row, col) => {
+  //   uniqueValues.clear();
+
+  //   //Find the coordinate for the first square in the box
+  //   let firstRow = Math.floor(row / squareSize) * squareSize;
+  //   let firstColumn = Math.floor(col / squareSize) * squareSize;
+
+  //   //Loop through the items in the square, and validate
+  //   for (let r = firstRow; r < firstRow + squareSize; r++) {
+  //     for (let c = firstColumn; c < firstColumn + squareSize; c++) {
+  //       let value = puzzleBoard[r][c];
+  //       if (isValidValue(value)) {
+  //         if (!uniqueValues.has(value)) {
+  //           uniqueValues.add(value);
+  //         } else {
+  //           return false;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return true;
+  // };
+
+  const isValidRow = (rowIndex) => {
     uniqueValues.clear();
 
-    //Find the coordinate for the first square in the box
-    let firstRow = Math.floor(row / squareSize) * squareSize;
-    let firstColumn = Math.floor(col / squareSize) * squareSize;
+    if (!puzzleBoard || puzzleBoard.length <= 0 || maximumValue <= rowIndex)
+      return false;
 
-    //Loop through the items in the square, and validate
-    for (let r = firstRow; r < firstRow + squareSize; r++) {
-      for (let c = firstColumn; c < firstColumn + squareSize; c++) {
-        let value = puzzleBoard[r][c];
-        if (isValidValue(value)) {
-          if (!uniqueValues.has(value)) {
-            uniqueValues.add(value);
-          } else {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  };
+    let items = puzzleBoard.slice(
+      rowIndex * maximumValue,
+      rowIndex * maximumValue + maximumValue
+    );
 
-  //Loop through the specified row index, and determine if it is currently valid
-  const isValidRow = (row) => {
-    uniqueValues.clear();
-    puzzleBoard[row].forEach((value) => {
+    let flag = true;
+    items.forEach((value) => {
       if (isValidValue(value)) {
         if (!uniqueValues.has(value)) {
           uniqueValues.add(value);
         } else {
-          return false;
+          flag = false;
         }
       }
     });
-    return true;
+    return flag;
   };
 
-  //Loop through the specified column index, and determine if it is currently valid
-  const isValidColumn = (column) => {
+  const isValidColumn = (columnIndex) => {
     uniqueValues.clear();
-    for (let i = 0; i < puzzleBoard.length; i++) {
-      let value = puzzleBoard[i][column];
+
+    if (!puzzleBoard || puzzleBoard.length <= 0 || maximumValue <= columnIndex)
+      return false;
+
+    let items = [];
+    for (let r = 0; r < maximumValue; r++) {
+      items = [...items, puzzleBoard[r * maximumValue + columnIndex]];
+    }
+
+    let flag = true;
+    items.forEach((value) => {
       if (isValidValue(value)) {
         if (!uniqueValues.has(value)) {
           uniqueValues.add(value);
         } else {
-          return false;
+          flag = false;
         }
       }
-    }
-    return true;
+    });
+    return flag;
   };
 
   const isValidValue = (value) => {
     return minimumValue <= value && value <= maximumValue;
   };
 
+  const startSolver = () => {
+    setStarted(true);
+  };
+
+  const flattenBoard = (board) => {
+    return board.flat();
+  };
+
+  const expandBoard = (flat) => {
+    //Create the multidimensional array to build the board
+    let board = [];
+    for (let r = 0; r < maximumValue; r++) {
+      let newRow = flat.slice(
+        r * maximumValue,
+        r * maximumValue + maximumValue
+      );
+      board = [...board, newRow];
+    }
+    return board;
+  };
+
+  const insertNextAnswer = () => {
+    let handle = setInterval(() => {
+      //Increment the index. The useEffect callback will handle updating the board
+      setInsertAnswerIndex((insertAnswerIndex) => insertAnswerIndex + 1);
+    }, 125);
+    setLoopHandle((loopHandle) => loopHandle + handle);
+  };
+
   useEffect(() => {
-    //Store the puzzle to solve into state
-    setPuzzleBoard(puzzles.easy[0]);
+    let index = insertAnswerIndex - 1;
+    if (0 <= index && index < puzzleBoard.length) {
+      let value = puzzleBoard[index];
+      if (!isValidValue(value)) {
+        //Insert a random number for now
+        let answer = Math.ceil(Math.random() * (maximumValue - 1));
+
+        setPuzzleBoard((puzzleBoard) => {
+          let newBoard = [...puzzleBoard];
+          newBoard[index] = answer;
+          return newBoard;
+        });
+      }
+    }
+
+    if (maximumValue * maximumValue <= index) {
+      console.log("clearInterval");
+      clearInterval(loopHandle);
+    }
+  }, [insertAnswerIndex]);
+
+  useEffect(() => {
+    if (started) {
+      insertNextAnswer();
+    }
+    return () => clearInterval(loopHandle);
+  }, [started]);
+
+  // useEffect(() => {
+  //   console.log({ validColumn: isValidSquare(4,5) });
+  // }, [puzzleBoard]);
+
+  useEffect(() => {
+    setPuzzleBoard(flattenBoard(puzzles.easy[0]));
   }, []);
 
   return (
     <div id="gameBody">
       <div id="gameboard">
-        {puzzleBoard.map((r, i) => {
+        {expandBoard(puzzleBoard).map((r, i) => {
           return (
             <div className="row" key={`row-${i}`}>
               {r.map((c, x) => {
