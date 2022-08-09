d77609b3762eff75dedfabb7a0e25ca63ce0a1d6
Sean Byrd <byrd.sean1015@gmail.com>
2022-08-08 20:17:58 -0400

    Added code to record the insertion indexes. This is to remember the previous indexes that we inserted into.

--- src/app/App.js
+++ src/app/App.js
@@ -15,10 +15,18 @@ function App() {
   const [started, setStarted] = useState(false);
 
   //Next index to insert an answer
-  const [insertAnswerIndex, setInsertAnswerIndex] = useState(0);
+  const [insertAnswerIndex, setInsertAnswerIndex] = useState(-1);
 
   //Handle for setInterval
   const [loopHandle, setLoopHandle] = useState(0);
+
+  //Record of all insertions to try and solve puzzle
+  const [insertionRecord, setInsertionRecord] = useState([]);
+
+  //Flag to know if the current board is valid
+  const [validBoard, setValidBoard] = useState(true);
+
+  const [outputMsg, setOutputMsg] = useState("");
 
   // //For a given row and col coordinate, validate the 3x3 square
   // const isValidSquare = (row, col) => {
@@ -44,13 +52,11 @@ function App() {
   //   return true;
   // };
 
-  const isValidRow = (rowIndex) => {
+  const isValidRow = (rowIndex, board) => {
     uniqueValues.clear();
+    if (!board || board.length <= 0 || maximumValue <= rowIndex) return false;
 
-    if (!puzzleBoard || puzzleBoard.length <= 0 || maximumValue <= rowIndex)
-      return false;
-
-    let items = puzzleBoard.slice(
+    let items = board.slice(
       rowIndex * maximumValue,
       rowIndex * maximumValue + maximumValue
     );
@@ -68,15 +74,15 @@ function App() {
     return flag;
   };
 
-  const isValidColumn = (columnIndex) => {
+  const isValidColumn = (columnIndex, board) => {
     uniqueValues.clear();
 
-    if (!puzzleBoard || puzzleBoard.length <= 0 || maximumValue <= columnIndex)
+    if (!board || board.length <= 0 || maximumValue <= columnIndex)
       return false;
 
     let items = [];
     for (let r = 0; r < maximumValue; r++) {
-      items = [...items, puzzleBoard[r * maximumValue + columnIndex]];
+      items = [...items, board[r * maximumValue + columnIndex]];
     }
 
     let flag = true;
@@ -117,32 +123,61 @@ function App() {
     return board;
   };
 
+  const getNextAnswerIndex = (index) => {
+    //Get the index for the next item to set
+    let newIndex = index + 1;
+    console.log({ newIndex, index });
+
+    //Keep looking for the next index to actually set, not one that already has a value
+    while (
+      0 <= newIndex &&
+      newIndex < puzzleBoard.length &&
+      puzzleBoard[newIndex] != 0
+    ) {
+      newIndex++;
+    }
+    return newIndex;
+  };
+
   const insertNextAnswer = () => {
     let handle = setInterval(() => {
       //Increment the index. The useEffect callback will handle updating the board
-      setInsertAnswerIndex((insertAnswerIndex) => insertAnswerIndex + 1);
-    }, 125);
-    setLoopHandle((loopHandle) => loopHandle + handle);
+      if (validBoard) {
+        // setInsertAnswerIndex((insertAnswerIndex) => insertAnswerIndex + 1);
+        setInsertAnswerIndex((insertAnswerIndex) =>
+          getNextAnswerIndex(insertAnswerIndex)
+        );
+      }
+    }, 1000);
+
+    //Set the handle
+    if (0 < handle) {
+      setLoopHandle((loopHandle) => loopHandle + handle);
+    }
   };
 
   useEffect(() => {
-    let index = insertAnswerIndex - 1;
-    if (0 <= index && index < puzzleBoard.length) {
-      let value = puzzleBoard[index];
+    if (0 <= insertAnswerIndex && insertAnswerIndex < puzzleBoard.length) {
+      let value = puzzleBoard[insertAnswerIndex];
       if (!isValidValue(value)) {
-        //Insert a random number for now
-        let answer = Math.ceil(Math.random() * (maximumValue - 1));
+        //Create a new board with the updated value
+        let newBoard = [...puzzleBoard];
+        newBoard[insertAnswerIndex] = value + 1;
 
-        setPuzzleBoard((puzzleBoard) => {
-          let newBoard = [...puzzleBoard];
-          newBoard[index] = answer;
-          return newBoard;
-        });
+        // Validate the new board
+        let row = Math.floor(insertAnswerIndex / maximumValue);
+        let column = insertAnswerIndex - row * maximumValue;
+        let isValid =
+          isValidRow(row, newBoard) && isValidColumn(column, newBoard);
+        setValidBoard(isValid);
+
+        //Update the puzzle board
+        setPuzzleBoard(newBoard);
       }
     }
 
-    if (maximumValue * maximumValue <= index) {
-      console.log("clearInterval");
+    //End the loop if there are no more values to loop through
+    if (maximumValue * maximumValue <= insertAnswerIndex) {
       clearInterval(loopHandle);
     }
   }, [insertAnswerIndex]);
@@ -154,9 +189,16 @@ function App() {
     return () => clearInterval(loopHandle);
   }, [started]);
 
-  // useEffect(() => {
-  //   console.log({ validColumn: isValidSquare(4,5) });
-  // }, [puzzleBoard]);
+  useEffect(() => {
+    if (0 <= insertAnswerIndex) {
+      //Record the index that was just worked on
+      setInsertionRecord((insertionRecord) => [
+        ...insertionRecord,
+        insertAnswerIndex,
+      ]);
+      setOutputMsg((outputMsg) => `${outputMsg} ${insertAnswerIndex}`);
+    }
+  }, [puzzleBoard]);
 
   useEffect(() => {
     setPuzzleBoard(flattenBoard(puzzles.easy[0]));
@@ -185,6 +227,7 @@ function App() {
       <div id="controls">
         <button onClick={startSolver}>Start</button>
         <button>Reset</button>
+        <p>{outputMsg}</p>
       </div>
     </div>
   );
