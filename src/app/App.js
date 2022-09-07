79134d9d286c4d04deb596b20a20ddd1fe80942f
Sean Byrd <byrd.sean1015@gmail.com>
2022-09-06 21:30:13 -0400

    Implemented functionality for reset button.

--- src/app/App.js
+++ src/app/App.js
@@ -25,6 +25,9 @@ function App() {
   //Flag to know if the solver started
   const [started, setStarted] = useState(false);
 
+  //Set the current puzzle value
+  const [currentPuzzleValue, setCurrentPuzzleValue] = useState("");
+
   //Create a new board with the updated value
   const setNewValueToBoard = (index, value) => {
     let newBoard = [...puzzleBoard];
@@ -44,70 +47,86 @@ function App() {
     return lastIndex;
   };
 
+  const selectPuzzle = (value) => {
+    if (value) {
+      setPuzzleBoard(flattenBoard(getPuzzle(value)));
+      setCurrentPuzzleValue(value);
+    }
+  };
+
   const startSolver = async () => {
     if (started) return;
 
     //Turn the "started" flag on
     setStarted(true);
+  };
 
-    //Keep looping until we reach end of board
-    let index = 0;
-    let resetPreviousValid = false;
-    while (true) {
-      //Check if the loop should break
-      if (puzzleBoard.length <= index) break;
+  const resetPuzzle = () => {
+    selectPuzzle(currentPuzzleValue);
+    setStarted(false);
+  };
 
-      //Check if the current index contains an original value from the board
-      //If so, continue
-      if (puzzleBoard[index].isOriginal) {
-        index++;
-        continue;
-      }
+  useEffect(() => {
+    const solve = async () => {
+      //Keep looping until we reach end of board
+      let index = 0;
+      let resetPreviousValid = false;
+      while (started) {
+        //Check if the loop should break
+        if (puzzleBoard.length <= index) break;
 
-      //Get the value of the current index. If the value is invalid, update the board
-      let value = puzzleBoard[index].value;
-      if (puzzleVals.maximumValue < value) {
-        //Reset the current value of the board
-        setNewValueToBoard(index, 0);
-
-        //Set the index to the LAST index that was set
-        index = findLastSetIndex(index);
-
-        //Set the flag to know that we need to update the previous valid value
-        resetPreviousValid = true;
-      } else if (!isValidValue(value)) {
-        setNewValueToBoard(index, value + 1);
-      } else {
-        //Value is within the valid range. Validate the board
-        let row = Math.floor(index / puzzleVals.maximumValue);
-        let column = index - row * puzzleVals.maximumValue;
-        let isValid =
-          !resetPreviousValid &&
-          isValidSquare(row, column, puzzleBoard) &&
-          isValidRow(row, puzzleBoard) &&
-          isValidColumn(column, puzzleBoard);
-
-        //if the board is valid, then move to the next index.
-        //Otherwise, set a new value
-        if (isValid) {
+        //Check if the current index contains an original value from the board
+        //If so, continue
+        if (puzzleBoard[index].isOriginal) {
           index++;
-        } else {
-          //Board is invalid. Increment the value and check in the next loop
-          setNewValueToBoard(index, value + 1);
-          resetPreviousValid = false;
+          continue;
         }
+
+        //Get the value of the current index. If the value is invalid, update the board
+        let value = puzzleBoard[index].value;
+        if (puzzleVals.maximumValue < value) {
+          //Reset the current value of the board
+          setNewValueToBoard(index, 0);
+
+          //Set the index to the LAST index that was set
+          index = findLastSetIndex(index);
+
+          //Set the flag to know that we need to update the previous valid value
+          resetPreviousValid = true;
+        } else if (!isValidValue(value)) {
+          setNewValueToBoard(index, value + 1);
+        } else {
+          //Value is within the valid range. Validate the board
+          let row = Math.floor(index / puzzleVals.maximumValue);
+          let column = index - row * puzzleVals.maximumValue;
+          let isValid =
+            !resetPreviousValid &&
+            isValidSquare(row, column, puzzleBoard) &&
+            isValidRow(row, puzzleBoard) &&
+            isValidColumn(column, puzzleBoard);
+
+          //if the board is valid, then move to the next index.
+          //Otherwise, set a new value
+          if (isValid) {
+            index++;
+          } else {
+            //Board is invalid. Increment the value and check in the next loop
+            setNewValueToBoard(index, value + 1);
+            resetPreviousValid = false;
+          }
+        }
+
+        //Sleep for a few seconds
+        await sleep(50);
       }
 
-      //Sleep for a few seconds
-      await sleep(50);
-    }
-  };
+      //Turn the "started" flag off
+      setStarted(false);
+    };
 
-  const selectPuzzle = (value) => {
-    if (value) {
-      setPuzzleBoard(flattenBoard(getPuzzle(value)));
-    }
-  };
+    //Solve the puzzle
+    solve();
+  }, [started]);
 
   useEffect(() => {
     if (puzzleOptions && 0 < puzzleOptions.length) {
@@ -142,6 +161,7 @@ function App() {
       </div>
       <Controls
         startSolver={startSolver}
+        resetPuzzle={resetPuzzle}
         selectPuzzle={selectPuzzle}
         randomId={randomId}
         puzzleOptions={puzzleOptions}
